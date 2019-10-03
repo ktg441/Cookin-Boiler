e2de906d24eaceaa0d9ab05ce0eacfb398662c2a
Kiran Thomas Payne <kiran.t.goel@gmail.com>
2019-10-03 12:48:25 -0400

    Set up Raycast script

---
+++ Assets/Scripts/Cutting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cutting : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 100))
+        {
+            //Hit colliders
+        }
+        
+    }
+
+    void cutObject()
+    {
+
+    }
+}
