028662eef8f33e0d268ece9676f0e628191e9d9d
Kiran Payne <kiran.t.goel@gmail.com>
2019-10-23 02:24:08 -0400

    Pot fills with water and releases water when tilted

---
+++ Assets/Scripts/Faucet.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Faucet : MonoBehaviour
+{
+
+    private bool state;
+    private GameObject water;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = false;
+        water = GameObject.Find("Running Water");
+        water.SetActive(state);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("GameController"))
+        {
+            water.SetActive(state);
+            state = !state;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Water.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Water : MonoBehaviour
+{
+    private GameObject water;
+    // Start is called before the first frame update
+    void Start()
+    {
+        water = GameObject.Find("Full");
+        water.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.eulerAngles.x > 45 && transform.eulerAngles.x < 135)
+        {
+            water.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Equals("Running Water"))
+        {
+            water.SetActive(true);
+        }
+    }
+}
