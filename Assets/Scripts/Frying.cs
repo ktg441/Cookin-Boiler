75c003e22198325fe6f19b0205f545c1658012bc
Kiran Payne <kiran.t.goel@gmail.com>
2019-10-24 10:08:51 -0400

    Set up placing objects in frying pan

---
+++ Assets/Scripts/Frying.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Frying : MonoBehaviour
+{
+    private GameObject attached;
+    // Start is called before the first frame update
+    void Start()
+    {
+        attached = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.eulerAngles.x > 45 && transform.eulerAngles.x < 135)
+        {
+            if (attached != null)
+            {
+                attached.GetComponent<Rigidbody>().isKinematic = false;
+                attached.transform.parent.SetParent(GameObject.Find("Interactables").transform.parent);
+                attached.tag = "Food";
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Food"))
+        {
+            //print(other.name);
+            other.GetComponent<Rigidbody>().isKinematic = true;
+            if (other.GetComponent<Interactable>().m_ActiveHand)
+            {
+                other.GetComponent<Interactable>().m_ActiveHand.m_ContactInteractables.Remove(other.gameObject.GetComponent<Interactable>());
+                other.GetComponent<Interactable>().m_ActiveHand.Drop();
+            }
+            other.tag = "Untagged";
+            other.transform.parent.SetParent(transform);
+            attached = other.gameObject;
+        }
+    }
+}
