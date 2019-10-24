f0efd796799bdf54ab6be24851a771a7b5ffc160
Kiran Thomas Payne <kiran.t.goel@gmail.com>
2019-10-24 11:45:59 -0400

    Heating up script complete, test on lab machine

--- Assets/Scripts/Properties.cs
+++ Assets/Scripts/Properties.cs
@@ -6,17 +6,31 @@ public class Properties : MonoBehaviour
 {
     public GameObject unfinished;
     public GameObject finished;
+
+    public int heatTime;
+    private int heated;
     // Start is called before the first frame update
     void Start()
     {
         unfinished = transform.GetChild(0).gameObject;
         finished = transform.GetChild(1).gameObject;
         finished.SetActive(false);
+        heated = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public int getHeat()
+    {
+        return heated;
+    }
+
+    public void setHeat(int newHeat)
+    {
+        heated = newHeat;
     }
 }
--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -6,12 +6,17 @@ public class Water : MonoBehaviour
 {
     private GameObject water;
     private GameObject attached;
+
+    private bool heating;
+    private int start, end;
     // Start is called before the first frame update
     void Start()
     {
         water = GameObject.Find("Full");
         water.SetActive(false);
         attached = null;
+        heating = false;
+        start = end = 0;
     }
 
     // Update is called once per frame
@@ -27,6 +32,11 @@ public class Water : MonoBehaviour
                 attached.tag = "Food";
             }
         }
+
+        if (heating)
+        {
+            start += (int)Time.deltaTime % 60;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -35,18 +45,44 @@ public class Water : MonoBehaviour
         {
             water.SetActive(true);
         }
-        if (other.CompareTag("Food") && water.activeInHierarchy)
+        if (water.activeInHierarchy)
         {
-            //print(other.name);
-            other.GetComponent<Rigidbody>().isKinematic = true;
-            if(other.GetComponent<Interactable>().m_ActiveHand)
+            if (other.CompareTag("Food"))
             {
-                other.GetComponent<Interactable>().m_ActiveHand.m_ContactInteractables.Remove(other.gameObject.GetComponent<Interactable>());
-                other.GetComponent<Interactable>().m_ActiveHand.Drop();          
+                //print(other.name);
+                other.GetComponent<Rigidbody>().isKinematic = true;
+                if (other.GetComponent<Interactable>().m_ActiveHand)
+                {
+                    other.GetComponent<Interactable>().m_ActiveHand.m_ContactInteractables.Remove(other.gameObject.GetComponent<Interactable>());
+                    other.GetComponent<Interactable>().m_ActiveHand.Drop();
+                }
+                other.tag = "Untagged";
+                other.transform.parent.SetParent(transform);
+                attached = other.gameObject;
             }
-            other.tag = "Untagged";
-            other.transform.parent.SetParent(transform);
-            attached = other.gameObject;
+
+            if (other.CompareTag("Heat"))
+            {
+                heating = true;
+                start = other.transform.parent.GetComponent<Properties>().getHeat();
+                end = other.transform.parent.GetComponent<Properties>().heatTime;
+            }
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (water.activeInHierarchy)
+        {
+            if (other.CompareTag("Heat"))
+            {
+                print("Current time: " + start);
+                heating = false;
+                other.transform.parent.GetComponent<Properties>().setHeat(start);
+                start = 0;
+                end = 0;
+            }
         }
     }
 
